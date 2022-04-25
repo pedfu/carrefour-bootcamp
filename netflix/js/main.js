c6d24f9d9c7e06dd92379ebb465c854171792c54
Pedro Fuziwara <fuziwarapedro@gmail.com>
2022-04-25 10:10:34 -0300

    adding functionality to buttons

--- netflix/js/main.js
+++ netflix/js/main.js
@@ -14,3 +14,27 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+let closed = true;
+let video = document.getElementById("video");
+let popup = document.getElementById("popup");
+
+function clicked() {
+    if(closed === true) {
+        popup.classList.remove("disabled");
+        closed = false;
+        stopVideo();
+    } else {
+        popup.classList.add("disabled");
+        closed = true;
+        stopVideo();
+    }
+
+    console.log(closed);
+}
+
+function stopVideo() {
+    const source = video.src;
+    video.src = '';
+    video.src = source;
+}
